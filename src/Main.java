f2752dad4250f0499cce356e14e3ac491d640de5
User <git@github.com:gdsStern/HelloWorld.git>
2024-02-21 20:04:41 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -26,7 +26,6 @@ public class Main {
         }
 
     }
-
     public static boolean isYearLeap(int year) {
         return ((year % 4 == 0 && year % 100 != 0 || year % 400 == 0) && year>=1584);
     }
